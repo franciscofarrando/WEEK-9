82b2ff31b3e5b3300585f68bef0c165de2e1a5ed
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-06 20:55:38 +0100

    mas ordenado

--- RANDOM/E1/main.js
+++ RANDOM/E1/main.js
@@ -1,18 +1,15 @@
 let number = parseInt(prompt("Di un numero del 1 al 10 y ganale a la computadora"));
 let random = Math.floor(Math.random() * 10);
-
 let times = "0";
 
 while(true){
+    times++;
 
-
-times++;
-
-if (number != random) {
-    number = parseInt(prompt("NOP! tu dijiste " + number + " y no lo era; di otro numero del 1 al 10 y ganale a la computadora. Lo has intentado: " + times));
-    
-} else {
-    alert("Acertaste en " + times + " veces. El numero RANDOM era: "+random);
-    break;
-}
+    if (number != random) {
+        number = parseInt(prompt("NOP! tu dijiste " + number + " y no lo era; di otro numero del 1 al 10 y ganale a la computadora. Lo has intentado: " + times));
+        
+    } else {
+        alert("Acertaste en " + times + " veces. El numero RANDOM era: "+random + ".");
+        break;
+    }
 }
