ea6aaad2416432c55db0ba6953f54ada50c096da
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-07 13:11:00 +0100

    base de dados funcionando con alerts.

---
+++ RANDOM/E2/mains.js
@@ -0,0 +1,18 @@
+player1 = Math.floor(Math.random() * 6)+1;
+player2 = Math.floor(Math.random() * 6)+1;
+
+
+while(true){
+
+    if (player1 > player2) {
+        alert("GANO PLAYER 1 con: " + player1 + " sobre PLAYER 2 con: " + player2);
+        break;
+    }else if (player1 < player2) {
+        alert("GANO PLAYER 2 con: " + player2 + " sobre PLAYER 1 con: " + player1);
+        break;  
+
+    } else{
+        alert("Empate. PLAYER 1: " + player1 + " y PLAYER 2: " +player2);
+        
+    }
+}
