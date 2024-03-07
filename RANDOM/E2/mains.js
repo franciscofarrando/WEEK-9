2839608ed425c8a4da73f6c2910be8b417379eaa
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-07 15:17:41 +0100

    me rindo, estoy muy cerca. no arranca de 0, sino que lanza y da resultado. no puedo programar bien los lanzamientos.

--- RANDOM/E2/mains.js
+++ RANDOM/E2/mains.js
@@ -1,16 +1,17 @@
-
-   
    function infoCaption1(){
-   let player1 = Math.floor(Math.random() * 6)+1;
+
+    player1 = Math.floor(Math.random() * 6)+1;
     document.getElementById("result1").innerHTML = player1;
     return player1;
    }
+   
+
 
     function infoCaption2(){
-    let player2 = Math.floor(Math.random() * 6)+1;
+
+    player2 = Math.floor(Math.random() * 6)+1;
     document.getElementById("result2").innerHTML = player2;
     return player2;
-
 }
 
 function finalResult() {
@@ -18,15 +19,14 @@ function finalResult() {
     let player2 = infoCaption2();
 
         if (player1 > player2) {
-            document.getElementById("resultado").innerHTML = ("GANO PLAYER 1 con: " + player1 + " sobre PLAYER 2 con: " + player2);
-           
+            document.getElementById("resultado").innerHTML = ("GANO JUGADOR 1 con: " + player1 + " sobre JUGADOR 2 con: " + player2);
+            return;           
         }else if (player1 < player2) {
-            document.getElementById("resultado").innerHTML = ("GANO PLAYER 2 con: " + player2 + " sobre PLAYER 1 con: " + player1);
-           
-
+            document.getElementById("resultado").innerHTML = ("GANO JUGADOR 2 con: " + player2 + " sobre JUGADOR 1 con: " + player1);
+            return;
         } else{
-            document.getElementById("resultado").innerHTML = ("Empate. PLAYER 1: " + player1 + " y PLAYER 2: " +player2);
-            
+            document.getElementById("resultado").innerHTML = ("Empate. JUGADOR 1: " + player1 + " y JUGADOR 2: " +player2);
+            return;
         }
     }
     finalResult();
