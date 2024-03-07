625dcf33bc643889789c02e84d4c01e9a2d3d890
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-03-07 14:59:45 +0100

    mas o menos bva tirando pero los botones lanzar no funcionan

--- RANDOM/E2/mains.js
+++ RANDOM/E2/mains.js
@@ -1,18 +1,33 @@
-player1 = Math.floor(Math.random() * 6)+1;
-player2 = Math.floor(Math.random() * 6)+1;
 
+   
+   function infoCaption1(){
+   let player1 = Math.floor(Math.random() * 6)+1;
+    document.getElementById("result1").innerHTML = player1;
+    return player1;
+   }
 
-while(true){
+    function infoCaption2(){
+    let player2 = Math.floor(Math.random() * 6)+1;
+    document.getElementById("result2").innerHTML = player2;
+    return player2;
 
-    if (player1 > player2) {
-        alert("GANO PLAYER 1 con: " + player1 + " sobre PLAYER 2 con: " + player2);
-        break;
-    }else if (player1 < player2) {
-        alert("GANO PLAYER 2 con: " + player2 + " sobre PLAYER 1 con: " + player1);
-        break;  
-
-    } else{
-        alert("Empate. PLAYER 1: " + player1 + " y PLAYER 2: " +player2);
-        break;
-    }
 }
+
+function finalResult() {
+    let player1 = infoCaption1();
+    let player2 = infoCaption2();
+
+        if (player1 > player2) {
+            document.getElementById("resultado").innerHTML = ("GANO PLAYER 1 con: " + player1 + " sobre PLAYER 2 con: " + player2);
+           
+        }else if (player1 < player2) {
+            document.getElementById("resultado").innerHTML = ("GANO PLAYER 2 con: " + player2 + " sobre PLAYER 1 con: " + player1);
+           
+
+        } else{
+            document.getElementById("resultado").innerHTML = ("Empate. PLAYER 1: " + player1 + " y PLAYER 2: " +player2);
+            
+        }
+    }
+    finalResult();
+
